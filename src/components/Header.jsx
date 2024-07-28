cfdabd13b278ba43378129ca0621a08d93c8a23d
AntonioMRH <81431693+AntonioMRH@users.noreply.github.com>
2024-07-28 12:37:53 -0300

    feat: add clear button to search input in Header
    - Implemented a button to clear the search input and redirect to home;
    - Updated handleSearch to manage the search input state;
    - Adjusted handleHomeClick to clear the input value and navigate to home.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,72 @@
-import { Link, NavLink } from "react-router-dom"
-import { useSelector } from 'react-redux'
-
-import '../styles/header.scss'
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useCallback, useState } from "react";
+import "../styles/header.scss";
 
 const Header = ({ searchMovies }) => {
-  
-  const { starredMovies } = useSelector((state) => state.starred)
+  const starredMovies = useSelector((state) => state.starred.starredMovies);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = useCallback(
+    (e) => {
+      setSearchValue(e.target.value);
+      searchMovies(e.target.value);
+    },
+    [searchMovies]
+  );
+
+  const handleHomeClick = useCallback(() => {
+    searchMovies("");
+    setSearchValue("");
+    navigate("/");
+  }, [searchMovies, navigate]);
 
   return (
     <header>
-      <Link to="/" data-testid="home" onClick={() => searchMovies('')}>
+      <Link to="/" data-testid="home" onClick={handleHomeClick}>
         <i className="bi bi-film" />
       </Link>
 
       <nav>
-        <NavLink to="/starred" data-testid="nav-starred" className="nav-starred">
+        <NavLink
+          to="/starred"
+          data-testid="nav-starred"
+          className="nav-starred"
+        >
           {starredMovies.length > 0 ? (
             <>
-            <i className="bi bi-star-fill bi-star-fill-white" />
-            <sup className="star-number">{starredMovies.length}</sup>
+              <i className="bi bi-star-fill bi-star-fill-white" />
+              <sup className="star-number">{starredMovies.length}</sup>
             </>
           ) : (
             <i className="bi bi-star" />
           )}
         </NavLink>
         <NavLink to="/watch-later" className="nav-fav">
-          watch later
+          Watch Later
         </NavLink>
       </nav>
 
       <div className="input-group rounded">
-        <Link to="/" onClick={(e) => searchMovies('')} className="search-link" >
-          <input type="search" data-testid="search-movies"
-            onKeyUp={(e) => searchMovies(e.target.value)} 
-            className="form-control rounded" 
-            placeholder="Search movies..." 
-            aria-label="Search movies" 
-            aria-describedby="search-addon" 
-            />
-        </Link>            
-      </div>      
+        <input
+          type="search"
+          data-testid="search-movies"
+          value={searchValue}
+          onChange={handleSearch}
+          className="form-control rounded"
+          placeholder="Search movies..."
+          aria-label="Search movies"
+          aria-describedby="search-addon"
+        />
+        {searchValue && (
+          <button className="btn btn-clear" onClick={handleHomeClick}>
+            <i className="bi bi-x" />
+          </button>
+        )}
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
